3fbce253700ce5ba230ef78d8404cdd1a3709c3d
Tonio101 <antonio@antonio101.com>
2022-03-03 00:02:11 -0800

    Add Home Id

--- src/utils/yolink_utils.py
+++ src/utils/yolink_utils.py
@@ -1,7 +1,9 @@
 import argparse
 import json
 import os
+import requests
 import sys
+import time
 
 from logging import DEBUG
 from logger import Logger
@@ -23,6 +25,54 @@ def parse_config_file(fname) -> dict:
     with open(os.path.abspath(fname), 'r') as fp:
         data = json.load(fp)
         return data
+
+
+def get_all_devices(apiUrl: str, access_token: str) -> dict:
+    data = dict()
+    headers = dict()
+
+    headers['Content-type'] = 'application/json'
+    headers['Authorization'] = 'Bearer ' + access_token
+
+    data['method'] = 'Home.getDeviceList'
+    data['time'] = str(int(time.time()*1000))
+
+    r = requests.post(apiUrl, data=json.dumps(data), headers=headers)
+
+    if r.status_code != 200:
+        log.error("Failed to get device list")
+        return {}
+
+    info = r.json()
+
+    if info['data'] and 'devices' in info['data']:
+        return info['data']['devices']
+
+    return {}
+
+
+def get_home_id(apiUrl: str, access_token: str) -> str:
+    data = dict()
+    headers = dict()
+
+    headers['Content-type'] = 'application/json'
+    headers['Authorization'] = 'Bearer ' + access_token
+
+    data['method'] = 'Home.getGeneralInfo'
+    data['time'] = str(int(time.time()*1000))
+
+    r = requests.post(apiUrl, data=json.dumps(data), headers=headers)
+    if r.status_code != 200:
+        log.error("Failed to get device list")
+        return ""
+
+    info = r.json()
+    if ('code' in info and info['code'] != '000000'):
+        log.error("Failed to get device list")
+        return ""
+
+    log.info(info)
+    return info['data']['id']
 
 
 def main(args):
@@ -48,16 +98,23 @@ def main(args):
 
     config = parse_config_file(args.config)
     log.debug(config)
-    yolink_token = \
-        YoLinkToken(url=config['yoLink']['apiv2']['tokenUrl'],
-                    ua_id=config['yoLink']['apiv2']['uaId'],
-                    sec_id=config['yoLink']['apiv2']['secId'])
-    token = yolink_token.get_access_token()
-    log.debug(yolink_token)
-    log.debug(token)
-    token = yolink_token.renew_token()
-    log.debug(yolink_token)
-    log.debug(token)
+
+    if args.devices:
+        yolink_token = \
+            YoLinkToken(url=config['yoLink']['apiv2']['tokenUrl'],
+                        ua_id=config['yoLink']['apiv2']['uaId'],
+                        sec_id=config['yoLink']['apiv2']['secId'])
+        token = yolink_token.get_access_token()
+        log.debug(yolink_token)
+
+        devices = get_all_devices(apiUrl=config['yoLink']['apiv2']['apiUrl'],
+                                  access_token=token)
+        for device in devices:
+            log.info(device)
+
+        home_id = get_home_id(apiUrl=config['yoLink']['apiv2']['apiUrl'],
+                              access_token=token)
+        log.info(home_id)
 
 
 if __name__ == '__main__':
