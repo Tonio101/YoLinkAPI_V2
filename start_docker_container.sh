ae0637ec62642d61171f980f0a17f3c21eed1076
Tonio101 <antonio@antonio101.com>
2022-08-31 23:36:21 -0700

    Minor change to docker file

--- start_docker_container.sh
+++ start_docker_container.sh
@@ -4,4 +4,4 @@ IMAGE_TAG=yolinkv2ha
 IMAGE_ID=$(docker images -q $IMAGE_TAG)
 
 
-docker run -d --network=host --restart=on-failure:5 ${IMAGE_ID}
+docker run -d --network=host --restart=on-failure ${IMAGE_ID}
